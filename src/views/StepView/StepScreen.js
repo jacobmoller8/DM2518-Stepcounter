61ad1aaaa933c3e501908423c3d6b24f53023eb4
Jacob Moller <jacob.moller8@hotmail.com>
2019-05-20 22:48:29 +0200

    design and placeholder for components

--- src/views/StepView/StepScreen.js
+++ src/views/StepView/StepScreen.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Button, Platform, NativeAppEventEmitter, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, Button, Platform, NativeAppEventEmitter, TouchableOpacity, Dimensions } from "react-native";
 import { withNavigation } from 'react-navigation';
 import { connect } from "react-redux";
 import { store } from '../../redux/store/store'
 import { initAppleHK } from '../../redux/actions/stepActions'
+
+import Icon from "react-native-vector-icons/MaterialIcons";
+import Icon2 from "react-native-vector-icons/MaterialCommunityIcons";
 
 class StepScreen extends Component {
 	constructor(props) {
@@ -12,13 +15,17 @@ class StepScreen extends Component {
 			steps: 0,
 			error: "working",
 			avg: 0,
-			stepObserver: null
+			stepObserver: null,
+			date: "",
+			month: ""
+
 		}
 	}
 	componentWillMount() {
 		if (Platform.OS === "ios") {
 			this.props.initAppleHK;
 		}
+		this.getCurrentDate()
 	}
 
 	componentWillReceiveProps(nextProp) {
@@ -85,7 +92,13 @@ class StepScreen extends Component {
 
 			}
 		})
+	}
 
+	getCurrentDate = () => {
+		this.date = new Date().getDate();
+		this.month = new Date().getMonth();
+		this.months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+		this.setState({ date: this.date, month: this.months[this.month] })
 	}
 
 
@@ -99,14 +112,35 @@ class StepScreen extends Component {
 
 		return (
 			<View style={styles.flexView}>
-				<Text style={styles.stepFont}>Number of steps: {this.state.steps}</Text>
+
+				<Text style={styles.date}>{this.state.date} {this.state.month}</Text>
+
+
+				<View style={styles.stepProgressBar}>
+					<Text>Todo Component</Text>
+				</View>
+				<View style={styles.profilePicRow}>
+					<View style={styles.line1}></View>
+					<TouchableOpacity style={styles.profilePic}>
+						<Icon name="person" color={"#525252"} size={60}></Icon>
+					</TouchableOpacity>
+					<View style={styles.line2}></View>
+				</View>
+
+				<Text style={styles.stepFont}>{this.state.steps}</Text>
+				<Text style={styles.stepsToUseLabel}>steps to use</Text>
+
 				<Text style={styles.avgFont}>Daily Average: {this.state.avg}</Text>
 				<Text style={curStyle}>Status: {this.state.error}</Text>
+
+				<TouchableOpacity style={styles.compensateButton}>
+					<Text style={styles.compensateText}>CLIMATE COMPENSATE</Text>
+					<Icon2 name="leaf" style={styles.compensateIcon} color={"#89B037"} size={30}></Icon2>
+				</TouchableOpacity>
 			</View>
 		)
 	}
 }
-
 
 const mapStateToProps = state => {
 	return {
@@ -124,20 +158,61 @@ const mapDispatchToProps = dispatch => {
 
 export default withNavigation(connect(mapStateToProps, mapDispatchToProps)(StepScreen));
 
-
-
+const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
 	flexView: {
 		flex: 1,
 		flexDirection: "column",
 		alignItems: "center",
-		justifyContent: "center",
+	},
+	date: {
+		marginTop: 40,
+		fontSize: 35,
+		color: "#525252"
+	},
+	stepProgressBar: {
+		marginTop: 10,
+		width: screenWidth * 0.9,
+		height: 70,
+		backgroundColor: "#425891"
+	},
+	profilePicRow: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: 'flex-start',
+		marginTop: -7
+	},
+	profilePic: {
+		height: 80,
+		width: 80,
+		backgroundColor: "white",
+		borderRadius: 50,
+		borderWidth: 1,
+		borderColor: "#525252",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	line1: {
+		width: 10,
+		height: 1,
+		backgroundColor: "#525252"
+	},
+	line2: {
+		width: screenWidth - 90,
+		height: 1,
+		backgroundColor: "#525252"
 	},
 	stepFont: {
+		marginTop: 30,
+		fontSize: 100,
+		color: "#425891",
+	},
+	stepsToUseLabel: {
+		marginTop: -20,
 		fontSize: 20,
 		color: "#525252",
-		fontWeight: 'bold'
 	},
 	avgFont: {
 		fontSize: 16,
@@ -151,6 +226,33 @@ const styles = StyleSheet.create({
 	errorFont: {
 		fontSize: 14,
 		color: "red"
+	},
+	compensateButton: {
+		backgroundColor: "white",
+		width: screenWidth * 0.8,
+		marginTop: 30,
+		height: 60,
+		borderRadius: 50,
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "center",
+		shadowColor: "#000000",
+		shadowOffset: {
+			width: 2,
+			height: 4,
+		},
+		shadowOpacity: 0.50,
+		shadowRadius: 4,
+		elevation: 12,
+	},
+	compensateText: {
+		marginLeft: 10,
+		fontSize: 20,
+		color: "#89B037"
+	},
+	compensateIcon: {
+		marginLeft: 10,
+		marginRight: 10
 	}
 });
 
