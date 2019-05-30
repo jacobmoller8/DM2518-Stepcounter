087affe76ec545703f0504483a511083e830c50d
hugobergqvist <hugo@bergqvist.com>
2019-05-30 14:38:21 +0200

    Delete firebaseConfig.js

--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCS7vvYOFnnKRn1cTpOq_Lg9mA3Lz9fKGs",
-    authDomain: "dm2518-stepcounter.firebaseapp.com",
-    databaseURL: "https://dm2518-stepcounter.firebaseio.com",
-    projectId: "dm2518-stepcounter",
-    storageBucket: "dm2518-stepcounter.appspot.com",
-    messagingSenderId: "209322474723",
-    appId: "1:209322474723:web:051274ee884a5c71"
-};
